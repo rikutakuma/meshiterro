f683c44746647225fe7c0529bb5d4115d913f065
榎本拓真 <t3460515@gmail.com>
2020-08-19 14:05:48 +0900

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '5fb1d9a1db37ac85957400cd54d500dfad3f13e264e33621265ed2356c944bfc64425dce49767cf4c560b7919af321f0bad59ec3acc53a8fdd98bcb139fc35b0'
